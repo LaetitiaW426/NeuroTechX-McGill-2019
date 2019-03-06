20f9da0fd70bb87d68cccb2ad403f1bb90ba0c2b
Raffi Hotter <rhotter@users.noreply.github.com>
2019-03-06 12:32:07 -0500

    added fft

--- src/dashboard-node/server.js
+++ src/dashboard-node/server.js
@@ -7,8 +7,8 @@ const io = require('socket.io').listen(server);
 const fs = require('fs');
 
 
-const createCsvWriter = require('csv-writer').createObjectCsvWriter;
-var csvWriter;
+const createCSVWriter = require('csv-writer').createObjectCsvWriter;
+var csvTimeWriter;
 
 //Will determine if collecting and sending to file currently.
 //Other values will only be updated if collecting is true!
@@ -16,7 +16,18 @@ var collecting = false;
 var duration = 0;
 var direction = "none";
 var active = [];
-const header = {time: 'Time',
+
+const timeHeader = [{id: 'time', title: 'TIME'},
+                    {id: 'channel1', title: 'CHANNEL 1'},
+                    {id: 'channel2', title: 'CHANNEL 2'},
+                    {id: 'channel3', title: 'CHANNEL 3'},
+                    {id: 'channel4', title: 'CHANNEL 4'},
+                    {id: 'channel5', title: 'CHANNEL 5'},
+                    {id: 'channel6', title: 'CHANNEL 6'},
+                    {id: 'channel7', title: 'CHANNEL 7'},
+                    {id: 'channel8', title: 'CHANNEL 8'}]
+
+const timeHeaderToWrite = {time: 'Time',
                   channel1: 'Channel 1',
                   channel2: 'Channel 2',
                   channel3: 'Channel 3',
@@ -26,8 +37,19 @@ const header = {time: 'Time',
                   channel7: 'Channel 7',
                   channel8: 'Channel 8'
                 };
-var samples = [header];
+var timeSamples = [timeHeaderToWrite];
 
+const fftHeader = [{id: 'time', title: 'TIME'}];
+for (i=0; i<125; i++) {
+  fftHeader.push({id: 'f' + (i+1), title: (i+1) + 'Hz'})
+}
+
+const fftHeaderToWrite = {time: 'Time'};
+for (i=0; i<125; i++) {
+  fftHeaderToWrite['f' + (i+1)] = (i+1) + 'Hz';
+}
+
+var fftSamples = [fftHeaderToWrite];
 
 /* These are manual settings that we can use to keep track of testNumber as an example */
 var settings = JSON.parse(fs.readFileSync('data_settings.json', 'utf8'));
@@ -42,55 +64,69 @@ function getTimeValue() {
 }
 
 /* Sets the csvwriters to the correct paths! */
-function setupCSVWriters(){
+function setupCsvWriters(){
     let date = new Date();
     var day = date.getFullYear() + '-' + date.getMonth() + '-' +
                    date.getDate() + '-' + date.getHours() + '-' +
                    date.getMinutes() + '-' + date.getSeconds()
 
-    csvWriter = createCsvWriter({
-          path: __dirname + '/data/test-' + testNumber + '-' + direction + '-'
+    csvTimeWriter = createCsvWriter({
+          path: __dirname + '/data/time-test-' + testNumber + '-' + direction + '-'
                           + day + '.csv',
-          header: [
-              {id: 'time', title: 'TIME'},
-              {id: 'channel1', title: 'CHANNEL 1'},
-              {id: 'channel2', title: 'CHANNEL 2'},
-              {id: 'channel3', title: 'CHANNEL 3'},
-              {id: 'channel4', title: 'CHANNEL 4'},
-              {id: 'channel5', title: 'CHANNEL 5'},
-              {id: 'channel6', title: 'CHANNEL 6'},
-              {id: 'channel7', title: 'CHANNEL 7'},
-              {id: 'channel8', title: 'CHANNEL 8'}
-          ],
+          header: timeHeader,
           append: true
     });
+
+    let csvFFTWriters = [];
+    for (i=0; i<8; i++) {
+      csvFFTWriters.push(createCSVWriter({
+        path: __dirname + '/data/fft-' + (i+1) + '-test-' + testNumber + '-'
+                        + direction + '-' + day + '.csv',
+        header: fftHeader,
+        append: true
+      }));
+    }
 }
 
-/* When data is collecting, samples will also write to file! */
-function appendSample(data){
-  channelData = []
-  console.log(active);
+function appendSample(data, type){
+  /*
+  Write samples to CSV file when data is collecting
+  */
+
+  channelData = [];
   for (i = 0; i < 8; i++) {
     if (active[i] == 1) {
-        channelData[i] = data['data']['data'][i];
+        channelData[i] = data['data'][i];
     }
     else {
       channelData[i] = null;
     }
   }
 
+  if (type =='fft') {
+    let fftSamplesToPush = [];
+    for (i=0; i<8; i++) {
+      fftSamplesToPush.push({time: data['time']});
+      for (j=0; j<125; j++) {
+        fftSamplesToPush[i]['f' + (i+1)] = channelData[i][j];
+      }
+    }
+    fftSamples.push(fftSamplesToPush);
+  }
 
-  sampleToPush = {time: data['time'],
-                  channel1: channelData[0],
-                  channel2: channelData[1],
-                  channel3: channelData[2],
-                  channel4: channelData[3],
-                  channel5: channelData[4],
-                  channel6: channelData[5],
-                  channel7: channelData[6],
-                  channel8: channelData[7]
-                }
-  samples.push(sampleToPush);
+  else if (type == 'time') {
+    let timeSampleToPush = {time: data['time'],
+                    channel1: channelData[0],
+                    channel2: channelData[1],
+                    channel3: channelData[2],
+                    channel4: channelData[3],
+                    channel5: channelData[4],
+                    channel6: channelData[5],
+                    channel7: channelData[6],
+                    channel8: channelData[7]
+                  }
+    timeSamples.push(timeSampleToPush);
+  }
 }
 
 /* Updates test number on data_settings file */
@@ -104,13 +140,21 @@ function endTest(saved){
       console.log('Updated Test Number!');
       testNumber = settings['testNumber'];
     });
-    // [ {ENTRY 1}, {ENTRY 2}]
-    console.log(samples);
-    csvWriter.writeRecords(samples).then(() => {
-      console.log('Added some samples');
+
+    // fft writers
+    for (i = 0; i < 8; i++) {
+      csvFFTWriters[i].writeRecords(fftSamples[i]).then(() => {
+        console.log('Added some fft samples');
+      });
+    }
+
+    // time writer
+    csvTimeWriter.writeRecords(timeSamples).then(() => {
+      console.log('Added some time samples');
     });
   }
-  samples = [header];
+  timeSamples = [timeHeaderToWrite];
+  fftSamples = [fftHeaderToWrite];
 }
 
 /* Creates a UDP client to listen to the OpenBCI GUI */
@@ -138,34 +182,28 @@ UDPClient.prototype.onMessage = function(msg) {
 /* Creates UDP Client */
 var client = new UDPClient(12345, "127.0.0.1");
 
-/* On sample received runs the following code: */
 client.events.on('sample', function(data) {
-  // console.log(data);
-  /* If it is a fast fourier transform or timeseries value does the following */
-  if(data['type'] == 'fft'){
+  /*
+  When sample is received, appends to CSV and pings client.
+  Data Format: {
+                  'time': time,
+                  'eeg': {'data': [0.5,3,-5,40,5,32,8,1]}
+                      data[index] is the eeg value at sensor-index
+                }
+  */
 
-    /* Sends a socket to the client called fft that has all the data */
-    io.sockets.emit('fft', data);
-  }
-  else{
+  if (collecting) {
     let time = getTimeValue();
-    if(collecting){
-      //Then we want to write to proper test file!
-      let toWrite = {'time': time, 'data': data};
-      appendSample(toWrite);
+    let toWrite = {'time': time, 'data': data['data']};
+
+    if (data['type'] == 'fft') {
+      appendSample(toWrite, type="fft"); // write to file
+      io.sockets.emit('fft', {'time': time, 'eeg': data}); // send socket to client
     }
-
-
-    /* Sends a socket to the client called timeseries that has the data as well as time */
-    /* Data Format:
-                  {
-                    'time': time,
-                    'eeg': {
-                              'data': [0.5,3,-5,40,5,32,8,1] data[index] is the eeg value at sensor-index
-                           }
-                  }
-    */
-    io.sockets.emit('timeseries', {'time': time, 'eeg': data});
+    else {
+      appendSample(toWrite, type="time")
+      io.sockets.emit('timeseries', {'time': time, 'eeg': data}); // send socket to client
+    }
   }
 });
 
@@ -179,7 +217,7 @@ io.on('connection', function(socket){
     duration = collectsocket['duration'];
     direction = collectsocket['command'];
     active = collectsocket['sensors'];
-    setupCSVWriters();
+    setupCsvWriters();
     let timeLeft = duration;
     collecting = true;
 
